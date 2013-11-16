7062a3da2c2697ae59e2bac157427d97017e0190
Ravi Shanker <ravi.program@gmail.com>
2013-11-16 14:42:40 +1100

    adding initial structure.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyApp::Application.config.secret_token = '230c3ca33d33f37e40e781428904930a326573391034d71e09a42136f08424d1daef85c6b1b6d5e0bd9d8c45a1fd6fc285d9961535100639471cc0e51dbe26b7'
